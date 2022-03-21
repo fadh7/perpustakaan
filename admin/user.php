70a8cc8d3f7c31d159cc45d7187dfbb5cb3eb38f
fadh7 <1910817110001@mhs.ulm.ac.id>
2022-03-21 08:11:34 +0800

    Update 21 Maret 2022

--- admin/user.php
+++ admin/user.php
@@ -45,7 +45,7 @@ $result_user = $stmt_user->fetchAll();
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
                 <div class="sidebar-brand-icon">
                     <img class="logo-abbr" height="60px" src="./img/logobaru.png" alt="">
                 </div>
@@ -56,7 +56,7 @@ $result_user = $stmt_user->fetchAll();
             <hr class="sidebar-divider my-0">
             <!-- Nav Item - Dashboard -->
             <li class="nav-item">
-                <a class="nav-link" href="index.html">
+                <a class="nav-link" href="index.php">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
